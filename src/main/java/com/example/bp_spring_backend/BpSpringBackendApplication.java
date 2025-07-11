d820166b60ba04019b3fba6d48448cd6396b0300
hrabcakm123 <hrabcakm123@gmail.com>
2025-07-11 15:52:46 +0200

    Project initialization

---
+++ src/main/java/com/example/bp_spring_backend/BpSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bp_spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpSpringBackendApplication.class, args);
+	}
+
+}
