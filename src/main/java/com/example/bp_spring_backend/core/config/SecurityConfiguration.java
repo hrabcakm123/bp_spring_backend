3e0f806ac6c2db825fc77a5c5cfc5ec642b169b3
hrabcakm123 <hrabcakm123@gmail.com>
2026-05-25 14:15:43 +0200

    update: add Docker support

--- src/main/java/com/example/bp_spring_backend/core/config/SecurityConfiguration.java
+++ src/main/java/com/example/bp_spring_backend/core/config/SecurityConfiguration.java
@@ -109,8 +109,8 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:80"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
