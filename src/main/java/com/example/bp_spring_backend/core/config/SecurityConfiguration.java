0c97eaa268daf0ab0919873084a2df2412735396
hrabcakm123 <hrabcakm123@gmail.com>
2026-05-25 14:49:28 +0200

    bugfix: update SecurityConfiguration

--- src/main/java/com/example/bp_spring_backend/core/config/SecurityConfiguration.java
+++ src/main/java/com/example/bp_spring_backend/core/config/SecurityConfiguration.java
@@ -109,7 +109,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:80"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
